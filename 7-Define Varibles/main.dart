acec12bcd44bc3a7192caf1969822fe485fe16da
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:15:30 +0300

    Create f7

---
+++ 7-Define Varibles/main.dart
@@ -0,0 +1,19 @@
+void main() {
+//* Define Varibles
+//1
+  int num1;
+  num1 = 10;
+  print('num1 = $num1');
+//2
+  double num2, num3;
+  num2 = 10.0;
+  num3 = 20.0;
+  print('num2 = $num2 , num3= $num3');
+//3
+  String ssn = '1', name = 'omar';
+  print('name=$name , ssn=$ssn');
+//4
+  double salary = 1000;
+  salary = 2000;
+  print('salary=$salary');
+}

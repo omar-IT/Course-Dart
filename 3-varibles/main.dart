b7babbede39be51a5c838e6da27e311affad6a87
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:13:46 +0300

    Create f3

---
+++ 3-varibles/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  //*DataType
+  //String x=' ' ||""
+  print('----------------string--------------------');
+  String FirstName = 'Omar';
+  String LastName = "AL-Zoubi";
+  print(FirstName + ' ' + LastName);
+  print('the fiestname:$FirstName the lastname:$LastName');
+  print('----------------integer--------------------');
+  int age = 21;
+  print('the age =$age');
+  print('----------------double---------------------');
+  double salary = 10000.10;
+  print('the salary is $salary');
+  print('-----------------bool-----------------------');
+  bool isselected = false;
+  print('is selected = $isselected');
+}

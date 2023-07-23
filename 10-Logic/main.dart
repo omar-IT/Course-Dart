c21317f14fcf362c33ff21b6cf20db8b7d2949da
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:16:14 +0300

    Create f10

---
+++ 10-Logic/main.dart
@@ -0,0 +1,17 @@
+void main() {
+// * Logical and ---> &&
+  print('-------Logical AND-------');
+  print('true && false = ${true && false}');
+  print('true && true = ${true && true}');
+
+// * Logical or ---> ||
+  print('-------Logical OR-------');
+  print('true || false = ${false || true}');
+  print('false || false = ${false || false}');
+
+// * Logical or ---> !
+  print('-------Logical NOT-------');
+  print('!true  => ${!true}');
+  print('!false => ${!false}');
+
+}

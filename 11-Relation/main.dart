6c09d0c9642deed3262a86a562f036805d598059
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:16:24 +0300

    Create f11

---
+++ 11-Relation/main.dart
@@ -0,0 +1,19 @@
+void main() {
+// * Greater Then
+  print('Greater Then 5 > 3 : ${5 > 3}');
+
+// * Less Then
+  print('Lesss Then 5 < 3 : ${5 < 3}');
+
+// * Greater Then Or Equal
+  print('Greater Then Or Equal 5 >= 3 : ${5 >= 3}');
+
+// * Less Then Or Equal
+  print('Less Then Or Equal 5 <= 3 : ${5 <= 3}');
+
+// * Is Equal To
+  print('Is Equal To 5 == 3 : ${5 == 3}');
+
+// * Is Not Equal To
+  print('Is Not Equal To 5 != 3 : ${5 != 3}');
+}

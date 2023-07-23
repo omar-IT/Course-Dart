9bfe8fd4e8a2bbfd523ca408a2f43ff3852c9cc8
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:15:08 +0300

    Create f5

---
+++ 5-var/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  var x = 5;
+  print(x.runtimeType);
+  var firstname = 'omar';
+  print(firstname.runtimeType);
+  var y = 5.5;
+  print(y.runtimeType);
+  var boolian = true;
+  print(boolian.runtimeType);
+  print('omar');
+}

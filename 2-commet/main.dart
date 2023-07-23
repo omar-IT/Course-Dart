7be927b5794052c3ddcb8f10e4858b1518acaf68
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-07-23 23:13:35 +0300

    Create f2

---
+++ 2-commet/main.dart
@@ -0,0 +1,21 @@
+void main(){
+  //print name omar
+print('omar');
+// print name alzoubi
+print("alzoubi");
+///1
+///2
+///3
+///4
+/**
+ * 1
+ * 2
+ * 3
+ * 4
+ */
+// * hi
+// **hi
+// ! not use
+// ? how create method
+// TODO :explained that  @param
+}

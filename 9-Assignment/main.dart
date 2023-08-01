b4c466ef8f8bf26e8b252b6fcba4891053674933
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-08-01 17:18:11 +0300

    UPD f9

--- 9-Assignment/main.dart
+++ 9-Assignment/main.dart
@@ -1,26 +1,28 @@
-void main(){
+void main() {
 //* (=) --> Assignment
-int a=10 , b=20 , count=10;
+  int a = 10, b = 20, count = 10;
 
 //* Add & Assign
-count += b;   //count = count + b; ----> 30
-print('count=$count');
+  count += b; //count = count + b; ----> 30
+  print('count=$count');
 
 //* Sub & Assign
-a-=b;     //a=a-b; ----> -10
-print('a=$a');
+  a -= b; //a=a-b; ----> -10
+  print('a=$a');
 
 //* Multi &  Assign
-b*=count;    //b=b*count; ---->600
-print('b=$b');
+  b *= count; //b=b*count; ---->600
+  print('b=$b');
 
 //* Div & Assign
-count~/=b;  //count=count ~/b; ----> 0.05 ->0
-print('count=$count');
+  count ~/= b; //count=count ~/b; ----> 0.05 ->0
+  print('count=$count');
 
 //* Mood % Assign
-print('$a=${-a}');
-a=-a;
-a%=b;   //a=-a%b; ---> 10
-print('a=$a');
+  print('$a=${-a}');
+//a=-a%b; ---> 10
+//print(a);
+  a = -a;
+  a %= b;
+  print('a=$a');
 }

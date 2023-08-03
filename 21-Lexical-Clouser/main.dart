3c802b3f82111b7d9fab01bed6860c2c174aa7d5
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-08-03 19:55:48 +0300

    Create f21

---
+++ 21-Lexical-Clouser/main.dart
@@ -0,0 +1,85 @@
+void main() {
+  //f1(){
+  //int x = 10;
+  //print('f1 befor f2 :x = $x');
+  //f2(){
+  //x *= 2;
+  //print('f2 : x = $x');
+  //}
+  // f2();
+  //print('f1 after f2 : x = $x');
+  //return f2();
+  //}
+  //f1();
+
+  Function f1() {
+    int x = 10;
+    print('f1 befor f2 :x = $x');
+    int f2() {
+      x *= 2;
+      print('f2 : x = $x');
+      return x;
+    }
+
+    print('f1 after f2 : x = $x');
+    return f2;
+  }
+  //Function r = f1();
+  //print(r());
+  //print(f1()());
+
+  Function f2(int x) {
+    x = 10;
+    print('f1 befor f2 :x = $x');
+    int f2() {
+      x *= 2;
+      print('f2 : x = $x');
+      return x;
+    }
+
+    print('f1 after f2 : x = $x');
+    return f2;
+  }
+  //Function r = f1();
+  //print(r());
+  //print(f1()());
+
+  Function f3(int x) {
+    x = 10;
+    print('f1 befor f2 :x = $x');
+    int f2(int y) {
+      x *= y;
+      print('f2 : x = $x');
+      return x;
+    }
+
+    print('f1 after f2 : x = $x');
+    return f2;
+  }
+  //print(f3(10)(3));
+
+  Function f4(int x) {
+    x = 10;
+    print('f1 befor f2 :x = $x');
+    Function f3(int y) {
+      x *= y;
+      print('f2 : x = $x');
+      int f2(int z) {
+        x *= z;
+        return x;
+      }
+
+      return f2;
+    }
+
+    print('f1 after f2 : x = $x');
+    return f3;
+  }
+  //int r = f4(1)(2)(3);
+  //print ('r : $r');
+
+  //Function r = f4(1);
+  //print(r(2)(3));
+
+  print('value : ${f4(1)(2)(3)}');
+}

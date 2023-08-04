703395fab57dcb88a7f3eb7e7bccf13d19135cf9
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-08-04 21:58:16 +0300

    Create f25

---
+++ 25-for-loop/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  //int i;
+  for (int i = 0; i <= 5; i++) {
+    print('$i : Omar Hafez Khalil');
+  }
+  //print(i);
+
+  for (int i = 0; i <= 2; i++) {
+    for (int j = 0; j <= 2; j++) {
+      print('i=$i j=$j i*j=$i*$j=${i*j}');
+    }
+  }
+}

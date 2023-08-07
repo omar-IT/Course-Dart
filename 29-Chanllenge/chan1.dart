ecdd4b5c24938d2bb434fb9dc02ba1572857ec7a
Omar AL-Zoubi <mo5rb600@gmail.com>
2023-08-07 14:22:00 +0300

    Create chan1

---
+++ 29-Chanllenge/chan1.dart
@@ -0,0 +1,31 @@
+void main() {
+  /// Q1
+  /// int x = 'omar';
+  /// double y = true;
+  /// bool x = 5;
+  /// String result = 4.4;
+  /// solution:
+  String x = 'omar';
+  bool y = true;
+  int z = 5;
+  double result = 4.4;
+
+  /// Q2
+  /// String name = 'omar',int age = 21;
+  /// print ('name : name , age #age');
+  /// output (name : omar , age 21)
+  /// solution:
+  String name = 'omar';
+  int age = 21;
+  print('name : $name , age $age');
+  print('name : omar , age 21');
+
+  /// Q3
+  /// int n = 10;
+  /// if (n >= 5) print('Greater than or Equal 5');
+  /// else print(Less than 5')
+  /// solution:
+  int n = 10;
+  if (n >= 5) print('Greater than or Equal 5');
+  else print('Less than 5');
+}
